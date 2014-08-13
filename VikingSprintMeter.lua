2299b13b9af983ce50004f241a10f93429224dae
Crowdedlight <crowdedhd@gmail.com>
2014-08-14 00:31:50 +0200

    added sprintmeter to windowmanagement

--- VikingSprintMeter.lua
+++ VikingSprintMeter.lua
@@ -6,6 +6,7 @@ require "Window"
 require "GameLib"
 require "Apollo"
 
+local VikingLib
 local VikingSprintMeter = {}
 
 function VikingSprintMeter:new(o)
@@ -23,23 +24,54 @@ end
 function VikingSprintMeter:OnLoad()
 	self.xmlDoc = XmlDoc.CreateFromFile("VikingSprintMeter.xml")
 	self.xmlDoc:RegisterCallback("OnDocumentReady", self)
+	Apollo.RegisterEventHandler("WindowManagementReady"      , "OnWindowManagementReady"      , self)
+  Apollo.RegisterEventHandler("WindowManagementUpdate"     , "OnWindowManagementUpdate"     , self)
 end
 
 function VikingSprintMeter:OnDocumentReady()
 	if  self.xmlDoc == nil then
 		return
 	end
-	Apollo.RegisterTimerHandler("SprintMeterGracePeriod", "OnSprintMeterGracePeriod", self)
-	Apollo.RegisterEventHandler("VarChange_FrameCount", "OnFrame", self)
-	Apollo.RegisterEventHandler("Tutorial_RequestUIAnchor", 	"OnTutorial_RequestUIAnchor", self)
+	Apollo.RegisterTimerHandler("SprintMeterGracePeriod"   , "OnSprintMeterGracePeriod"    , self)
+	Apollo.RegisterEventHandler("VarChange_FrameCount"     , "OnFrame"                     , self)
+	Apollo.RegisterEventHandler("Tutorial_RequestUIAnchor" , "OnTutorial_RequestUIAnchor" , self)
 
-	self.wndMain = Apollo.LoadForm(self.xmlDoc, "SprintMeterFormVert", "FixedHudStratum", self)
+	self.wndMain = Apollo.LoadForm(self.xmlDoc, "VikingSprintMeterForm", "FixedHudStratum", self)
 	self.wndMain:Show(false, true)
-	self.xmlDoc = nil
 	--self.wndMain:SetUnit(GameLib.GetPlayerUnit(), 40) -- 1 or 9 are also good
 
 	self.bJustFilled = false
 	self.nLastSprintValue = 0
+
+	--Settings
+	if VikingLib == nil then
+    VikingLib = Apollo.GetAddon("VikingLibrary")
+  end
+
+  if VikingLib ~= nil then
+    self.db = VikingLib.Settings.RegisterSettings(self, "VikingSprintMeter", self:GetDefaults(), "Sprint Meter")
+  end
+end
+
+function VikingSprintMeter:OnWindowManagementReady()
+  Event_FireGenericEvent("WindowManagementAdd", { wnd = self.wndMain, strName = "Viking SprintBar"} )
+end
+
+function VikingSprintMeter:OnWindowManagementUpdate(tWindow)
+  if tWindow and tWindow.wnd and tWindow.wnd == self.wndMain then
+    local bMoveable = tWindow.wnd:IsStyleOn("Moveable")
+
+    tWindow.wnd:SetStyle("RequireMetaKeyToMove", bMoveable)
+    tWindow.wnd:SetStyle("IgnoreMouse", not bMoveable)
+  end
+end
+
+function VikingSprintMeter:GetDefaults()
+  return {
+    char = {
+    SprintMeterShow = false
+    }  
+  }
 end
 
 function VikingSprintMeter:OnFrame()
@@ -49,9 +81,10 @@ function VikingSprintMeter:OnFrame()
 	end
 
 	local bWndVisible = self.wndMain:IsVisible()
-	local nRunCurr = unitPlayer:GetResource(0)
-	local nRunMax = unitPlayer:GetMaxResource(0)
-	local bAtMax = nRunCurr == nRunMax or unitPlayer:IsDead()
+	local nRunCurr    = unitPlayer:GetResource(0)
+	local nRunMax     = unitPlayer:GetMaxResource(0)
+	local bAtMax      = nRunCurr == nRunMax or unitPlayer:IsDead()
+	local bShowSprint = self.db.char.SprintMeterShow
 
 	self.wndMain:FindChild("ProgBar"):SetMax(nRunMax)
 	self.wndMain:FindChild("ProgBar"):SetProgress(nRunCurr, bWndVisible and nRunMax or 0)
@@ -72,7 +105,7 @@ function VikingSprintMeter:OnFrame()
 		Apollo.StopTimer("SprintMeterGracePeriod")
 	end
 
-	self.wndMain:Show(not bAtMax or self.bJustFilled, not bAtMax)
+	self.wndMain:Show(bShowSprint or not bAtMax or self.bJustFilled, not bAtMax)
 end
 
 function VikingSprintMeter:OnSprintMeterGracePeriod()
@@ -94,6 +127,20 @@ function VikingSprintMeter:OnTutorial_RequestUIAnchor(eAnchor, idTutorial, strPo
 
 	Event_FireGenericEvent("Tutorial_RequestUIAnchorResponse", eAnchor, idTutorial, strPopupText, tRect)
 	end
+end
+
+---------------------------------------------------------------------------------------------------
+-- VikingSettings Functions
+---------------------------------------------------------------------------------------------------
+
+function VikingSprintMeter:OnSettingsSprintMeter( wndHandler, wndControl, eMouseButton )
+  self.db.char.SprintMeterShow = wndControl:IsChecked()
+end
+
+-- Called when the settings form needs to be updated so it visually reflects the options
+function VikingSprintMeter:UpdateSettingsForm(wndContainer)
+  --Show SprintMeter to set new position
+  wndContainer:FindChild("SprintMeterShow:Content:ShowSprintMeter"):SetCheck(self.db.char.SprintMeterShow)
 end
 
 
