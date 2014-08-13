b67ceecfb445bf97584a7cf3638cea5d78beb229
Crowdedlight <crowdedhd@gmail.com>
2014-08-14 00:44:39 +0200

    fixed more tab spacing

--- VikingSprintMeter.lua
+++ VikingSprintMeter.lua
@@ -32,18 +32,18 @@ function VikingSprintMeter:OnDocumentReady()
 	if  self.xmlDoc == nil then
 		return
 	end
-	Apollo.RegisterTimerHandler("SprintMeterGracePeriod"   , "OnSprintMeterGracePeriod"    , self)
-	Apollo.RegisterEventHandler("VarChange_FrameCount"     , "OnFrame"                     , self)
-	Apollo.RegisterEventHandler("Tutorial_RequestUIAnchor" , "OnTutorial_RequestUIAnchor" , self)
+  Apollo.RegisterTimerHandler("SprintMeterGracePeriod"   , "OnSprintMeterGracePeriod"    , self)
+  Apollo.RegisterEventHandler("VarChange_FrameCount"     , "OnFrame"                     , self)
+  Apollo.RegisterEventHandler("Tutorial_RequestUIAnchor" , "OnTutorial_RequestUIAnchor" , self)
 
-	self.wndMain = Apollo.LoadForm(self.xmlDoc, "VikingSprintMeterForm", "FixedHudStratum", self)
-	self.wndMain:Show(false, true)
-	--self.wndMain:SetUnit(GameLib.GetPlayerUnit(), 40) -- 1 or 9 are also good
+  self.wndMain = Apollo.LoadForm(self.xmlDoc, "VikingSprintMeterForm", "FixedHudStratum", self)
+  self.wndMain:Show(false, true)
+  --self.wndMain:SetUnit(GameLib.GetPlayerUnit(), 40) -- 1 or 9 are also good
 
-	self.bJustFilled = false
-	self.nLastSprintValue = 0
+  self.bJustFilled = false
+  self.nLastSprintValue = 0
 
-	--Settings
+  --Settings
   if VikingLib == nil then
     VikingLib = Apollo.GetAddon("VikingLibrary")
   end
@@ -75,58 +75,58 @@ function VikingSprintMeter:GetDefaults()
 end
 
 function VikingSprintMeter:OnFrame()
-	local unitPlayer = GameLib.GetPlayerUnit()
-	if not unitPlayer then
-		return
-	end
+  local unitPlayer = GameLib.GetPlayerUnit()
+  if not unitPlayer then
+    return
+  end
 
-	local bWndVisible = self.wndMain:IsVisible()
-	local nRunCurr    = unitPlayer:GetResource(0)
-	local nRunMax     = unitPlayer:GetMaxResource(0)
-	local bAtMax      = nRunCurr == nRunMax or unitPlayer:IsDead()
-	local bShowSprint = self.db.char.SprintMeterShow
+  local bWndVisible = self.wndMain:IsVisible()
+  local nRunCurr    = unitPlayer:GetResource(0)
+  local nRunMax     = unitPlayer:GetMaxResource(0)
+  local bAtMax      = nRunCurr == nRunMax or unitPlayer:IsDead()
+  local bShowSprint = self.db.char.SprintMeterShow
 
-	self.wndMain:FindChild("ProgBar"):SetMax(nRunMax)
-	self.wndMain:FindChild("ProgBar"):SetProgress(nRunCurr, bWndVisible and nRunMax or 0)
+  self.wndMain:FindChild("ProgBar"):SetMax(nRunMax)
+  self.wndMain:FindChild("ProgBar"):SetProgress(nRunCurr, bWndVisible and nRunMax or 0)
 
-	if self.nLastSprintValue ~= nRunCurr then
-		self.nLastSprintValue = nRunCurr
-	end
+  if self.nLastSprintValue ~= nRunCurr then
+    self.nLastSprintValue = nRunCurr
+  end
 
-	if bWndVisible and bAtMax and not self.bJustFilled then
-		self.bJustFilled = true
-		Apollo.StopTimer("SprintMeterGracePeriod")
-		Apollo.CreateTimer("SprintMeterGracePeriod", 0.4, false)
-		--self.wndMain:FindChild("ProgFlash"):SetSprite("sprResourceBar_Sprint_ProgFlash")
-	end
+  if bWndVisible and bAtMax and not self.bJustFilled then
+    self.bJustFilled = true
+    Apollo.StopTimer("SprintMeterGracePeriod")
+    Apollo.CreateTimer("SprintMeterGracePeriod", 0.4, false)
+    --self.wndMain:FindChild("ProgFlash"):SetSprite("sprResourceBar_Sprint_ProgFlash")
+  end
 
-	if not bAtMax then
-		self.bJustFilled = false
-		Apollo.StopTimer("SprintMeterGracePeriod")
-	end
+  if not bAtMax then
+    self.bJustFilled = false
+    Apollo.StopTimer("SprintMeterGracePeriod")
+  end
 
-	self.wndMain:Show(bShowSprint or not bAtMax or self.bJustFilled, not bAtMax)
+  self.wndMain:Show(bShowSprint or not bAtMax or self.bJustFilled, not bAtMax)
 end
 
 function VikingSprintMeter:OnSprintMeterGracePeriod()
-	Apollo.StopTimer("SprintMeterGracePeriod")
-	self.bJustFilled = false
-	if self.wndMain and self.wndMain:IsValid() then
-		self.wndMain:Show(false)
-	end
+  Apollo.StopTimer("SprintMeterGracePeriod")
+  self.bJustFilled = false
+  if self.wndMain and self.wndMain:IsValid() then
+    self.wndMain:Show(false)
+  end
 end
 
 ---------------------------------------------------------------------------------------------------
 -- Tutorial anchor request
 ---------------------------------------------------------------------------------------------------
 function VikingSprintMeter:OnTutorial_RequestUIAnchor(eAnchor, idTutorial, strPopupText)
-	if eAnchor == GameLib.CodeEnumTutorialAnchor.VikingSprintMeter then
+  if eAnchor == GameLib.CodeEnumTutorialAnchor.VikingSprintMeter then
 
-	local tRect = {}
-	tRect.l, tRect.t, tRect.r, tRect.b = self.wndMain:GetRect()
+  local tRect = {}
+  tRect.l, tRect.t, tRect.r, tRect.b = self.wndMain:GetRect()
 
-	Event_FireGenericEvent("Tutorial_RequestUIAnchorResponse", eAnchor, idTutorial, strPopupText, tRect)
-	end
+  Event_FireGenericEvent("Tutorial_RequestUIAnchorResponse", eAnchor, idTutorial, strPopupText, tRect)
+  end
 end
 
 ---------------------------------------------------------------------------------------------------
